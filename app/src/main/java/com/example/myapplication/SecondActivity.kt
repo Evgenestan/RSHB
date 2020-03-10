7c088785cd39312878b19f833b0e4c58dd6d9289
Evgenestan <61114102+evgenestan@users.noreply.github.com>
2020-03-10 17:14:17 +0300

    Добавление функции отправки данных на сервер

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,12 +1,32 @@
 package com.example.myapplication
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
+import kotlinx.coroutines.*
+import androidx.fragment.app.DialogFragment
+
+class MyDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage("Пожалуйста, подождите")
+
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+}
 
 class SecondActivity : AppCompatActivity() {
     val data_file = "data"
@@ -18,6 +38,23 @@ class SecondActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+
+
+        fun upload() = GlobalScope.launch(Dispatchers.IO) {
+            val job: String? = GlobalScope.async(Dispatchers.IO) {
+
+                "https://projecttest0.000webhostapp.com/test0"
+                    .httpGet(listOf("name" to Name.text.toString()
+                        ,"surname" to surname.text.toString()
+                        ,"middle_name" to middle_name.text.toString()
+                        ,"email" to email.text.toString()
+                        ,"phone" to phone.text.toString()
+                        ,"address" to address.text.toString()
+                    ))
+                    .responseString()
+            }.await().third.component1()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,7 +79,13 @@ class SecondActivity : AppCompatActivity() {
         enter.setOnClickListener() {  //кнопка отправить
 
 
+            val myDialogFragment = MyDialogFragment()
+            val manager = supportFragmentManager
+            myDialogFragment.show(manager, "myDialog")
 
+            upload()
+
+            //myDialogFragment.dialog?.cancel()
             editor.putString("name", Name.text.toString())
             editor.putString("surname", surname.text.toString())
             editor.putString("phone", phone.text.toString())
@@ -52,6 +95,7 @@ class SecondActivity : AppCompatActivity() {
 
 
             editor.apply()
+
             val CheckInput = Name.length() * surname.length() * phone.length() * middle_name.length() * email.length() * address.length() //проверка на отсутсвие незаполненых полей
             if(consent.isChecked and (CheckInput!=0)) {
                 startActivity(intent1);
